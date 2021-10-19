6894411085d8aed0189ab6ce071d649739b56321
michaelyinopen <michaelyinopen@gmail.com>
2021-10-19 21:53:12 +1100

    tooltip child div

--- src/components/JobSetEditor/JobSetEditorTitleBar/SaveJobSetButton.jsx
+++ src/components/JobSetEditor/JobSetEditorTitleBar/SaveJobSetButton.jsx
@@ -61,45 +61,47 @@ const CreateJobSetButton = () => {
       isLoading={isCreating}
     >
       <Tooltip title={tooltip} placement="bottom-end">
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.button}
-          disabled={isCreating}
-          onClick={() => {
-            editorDispatch(savingStep(currentStepIndex, true))
-            dispatch(createJobSetTakingThunkAction(createJobSetRequest, creationToken))
-              .then(result => {
-                if (result?.kind === 'success') {
-                  const createdJobSet = result.success()
-                  const createdId = createdJobSet.id
-                  dispatch(addNotification({
-                    summary: `Created Activity #${createdId}`
-                  }))
-                  editorDispatch(loadedJobSet())
-                  editorDispatch(savedStep(currentStepIndex))
-                  history.push(generatePath(routePaths.jobSetEditor, { id: createdId }))
-                }
-                else {
+        <div>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            disabled={isCreating}
+            onClick={() => {
+              editorDispatch(savingStep(currentStepIndex, true))
+              dispatch(createJobSetTakingThunkAction(createJobSetRequest, creationToken))
+                .then(result => {
+                  if (result?.kind === 'success') {
+                    const createdJobSet = result.success()
+                    const createdId = createdJobSet.id
+                    dispatch(addNotification({
+                      summary: `Created Activity #${createdId}`
+                    }))
+                    editorDispatch(loadedJobSet())
+                    editorDispatch(savedStep(currentStepIndex))
+                    history.push(generatePath(routePaths.jobSetEditor, { id: createdId }))
+                  }
+                  else {
+                    dispatch(addNotification({
+                      summary: 'Failed to create Job Set',
+                      matchPath: routePaths.newJobSet
+                    }))
+                    editorDispatch(savingStep(currentStepIndex, false))
+                  }
+                })
+                .catch(() => {
                   dispatch(addNotification({
                     summary: 'Failed to create Job Set',
                     matchPath: routePaths.newJobSet
                   }))
                   editorDispatch(savingStep(currentStepIndex, false))
-                }
-              })
-              .catch(() => {
-                dispatch(addNotification({
-                  summary: 'Failed to create Job Set',
-                  matchPath: routePaths.newJobSet
-                }))
-                editorDispatch(savingStep(currentStepIndex, false))
-              })
-          }}
-        >
-          <SaveIcon className={classes.saveIcon} />
-          Create
-        </Button>
+                })
+            }}
+          >
+            <SaveIcon className={classes.saveIcon} />
+            Create
+          </Button>
+        </div>
       </Tooltip >
     </ProgressOverlay>
   )
@@ -133,57 +135,59 @@ const UpdateJobSetButton = ({ id }) => {
       isLoading={isSaving}
     >
       <Tooltip title={tooltip} placement="bottom-end">
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.button}
-          disabled={disabled}
-          onClick={() => {
-            editorDispatch(savingStep(currentStepIndex, true))
-            dispatch(updateJobSetTakingThunkAction(id, updateJobSetRequest))
-              .then(result => {
-                if (result?.kind === 'success') {
-                  dispatch(addNotification({
-                    summary: `Saved Job Set #${id}`
-                  }))
-                  editorDispatch(savedStep(currentStepIndex))
-                  return
-                }
-                if (result?.failure().failureType === 'version condition failed') {
-                  dispatch(addNotification({
-                    summary: `Job Set #${id} was updated by another user, check the merged changes and save again`,
-                    matchPath: path
-                  }))
-                }
-                else if (result?.failure().failureType === 'forbidden because locked') {
-                  dispatch(addNotification({
-                    summary: `Job Set #${id} was locked and cannot be saved`,
-                    matchPath: path
-                  }))
-                }
-                else if (result?.failure().failureType === 'not found') {
-                  dispatch(addNotification({
-                    summary: `Job Set #${id} was deleted and cannot be saved`,
-                    matchPath: path
-                  }))
-                }
-                else {
-                  dispatch(addNotification({
-                    summary: `Failed to save Job Set #${id}`,
-                    matchPath: path
-                  }))
-                }
-                editorDispatch(savingStep(currentStepIndex, false))
-              })
-              .catch(() => {
-                dispatch(addNotification(`Failed to saved Job Set #${id}`))
-                editorDispatch(savingStep(currentStepIndex, false))
-              })
-          }}
-        >
-          <SaveIcon className={classes.saveIcon} />
-          Save
-        </Button>
+        <div>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            disabled={disabled}
+            onClick={() => {
+              editorDispatch(savingStep(currentStepIndex, true))
+              dispatch(updateJobSetTakingThunkAction(id, updateJobSetRequest))
+                .then(result => {
+                  if (result?.kind === 'success') {
+                    dispatch(addNotification({
+                      summary: `Saved Job Set #${id}`
+                    }))
+                    editorDispatch(savedStep(currentStepIndex))
+                    return
+                  }
+                  if (result?.failure().failureType === 'version condition failed') {
+                    dispatch(addNotification({
+                      summary: `Job Set #${id} was updated by another user, check the merged changes and save again`,
+                      matchPath: path
+                    }))
+                  }
+                  else if (result?.failure().failureType === 'forbidden because locked') {
+                    dispatch(addNotification({
+                      summary: `Job Set #${id} was locked and cannot be saved`,
+                      matchPath: path
+                    }))
+                  }
+                  else if (result?.failure().failureType === 'not found') {
+                    dispatch(addNotification({
+                      summary: `Job Set #${id} was deleted and cannot be saved`,
+                      matchPath: path
+                    }))
+                  }
+                  else {
+                    dispatch(addNotification({
+                      summary: `Failed to save Job Set #${id}`,
+                      matchPath: path
+                    }))
+                  }
+                  editorDispatch(savingStep(currentStepIndex, false))
+                })
+                .catch(() => {
+                  dispatch(addNotification(`Failed to saved Job Set #${id}`))
+                  editorDispatch(savingStep(currentStepIndex, false))
+                })
+            }}
+          >
+            <SaveIcon className={classes.saveIcon} />
+            Save
+          </Button>
+        </div>
       </Tooltip >
     </ProgressOverlay>
   )
