35f4fc53a7ebb5e11263fbd86c44b4fd7059510f
michaelyinopen <michaelyinopen@gmail.com>
2021-10-20 00:01:18 +1100

    refreshed step merge options

--- src/components/JobSetEditor/HistoryPanel/StepItem.jsx
+++ src/components/JobSetEditor/HistoryPanel/StepItem.jsx
@@ -1,4 +1,4 @@
-import { memo, useRef } from 'react'
+import { memo, useRef, useMemo, useState } from 'react'
 import clsx from 'clsx'
 import {
   makeStyles,
@@ -7,20 +7,30 @@ import {
   Divider,
   Button,
   Typography,
-  List,
   ListItem,
   ListItemText,
+  ListItemSecondaryAction,
   Collapse,
+  IconButton,
+  FormControl,
+  RadioGroup,
+  FormLabel,
+  FormControlLabel,
+  Radio,
 } from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import {
   useJobSetEditorSelector,
   createNormalStepSelector,
   createStepDoneStatusSelector,
+  createStepSelector,
   useJobSetEditorDispatch,
   jumpToStep,
+  setMergeBehaviourDiscardLocal,
+  setMergeBehaviourMerge,
 } from '../store'
 
-const useStyles = makeStyles(theme => createStyles({
+const useNormalStepItemStyles = makeStyles(theme => createStyles({
   step: {
     paddingLeft: theme.spacing(2),
     paddingRight: theme.spacing(2),
@@ -39,7 +49,7 @@ const useStyles = makeStyles(theme => createStyles({
 }))
 
 const NormalStepItem = ({ step, isCurrent, undone }) => {
-  const classes = useStyles()
+  const classes = useNormalStepItemStyles()
   const editorDispatch = useJobSetEditorDispatch()
   return (
     <ListItem
@@ -60,9 +70,134 @@ const NormalStepItem = ({ step, isCurrent, undone }) => {
   )
 }
 
+const useVersionedStepItemStyles = makeStyles(theme => createStyles({
+  step: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+  undoneStep: {
+    borderLeft: `${theme.spacing(1)}px dotted ${theme.palette.grey[300]}`,
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(2),
+    opacity: 0.5,
+  },
+  currentStep: {
+    borderLeft: `${theme.spacing(1)}px solid ${theme.palette.grey[300]}`,
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(2),
+  },
+  iconButton: {
+    padding: theme.spacing(1),
+  },
+  mergeOptions: {
+    paddingLeft: theme.spacing(3),
+    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+  },
+  expandMore: {
+    transition: `transform ${theme.transitions.duration.standard}ms ${theme.transitions.easing.easeInOut}`,
+    transform: 'rotate(0deg)',
+  },
+  expandLess: {
+    transition: `transform ${theme.transitions.duration.standard}ms ${theme.transitions.easing.easeInOut}`,
+    transform: 'rotate(180deg)',
+  },
+  radioButton: {
+    padding: theme.spacing(0.5),
+  },
+  smallFont: {
+    fontSize: '0.875rem',
+  }
+}))
+
 const VersionedStepItem = ({ normalStep, isCurrent, undone }) => {
-  // todo
-  return null
+  const stepId = normalStep.id
+  const [expand, setExpand] = useState(false)
+  const classes = useVersionedStepItemStyles()
+  const editorDispatch = useJobSetEditorDispatch()
+  const step = useJobSetEditorSelector(createStepSelector(stepId))
+
+  const showMergeOptions = useMemo(
+    () => step.operations.some(op => op.type === 'reverse local' || op.type === 'conflict'),
+    [step.operations]
+  )
+  const conflicts = useMemo(
+    () => step.mergeBehaviour !== 'merge'
+      ? []
+      : step.operations.filter(op => op.type === 'conflict'),
+    [step.mergeBehaviour, step.operations]
+  )
+
+  return (
+    <>
+      <ListItem
+        button
+        divider={!expand}
+        disableGutters
+        className={clsx({
+          [classes.currentStep]: isCurrent,
+          [classes.undoneStep]: undone,
+          [classes.step]: !isCurrent && !undone
+        })}
+        onClick={() => {
+          editorDispatch(jumpToStep(step.id))
+        }}
+      >
+        <ListItemText primary={step?.name ?? "\u00a0"} />
+        {showMergeOptions && (
+          <ListItemSecondaryAction>
+            <IconButton
+              className={classes.iconButton}
+              onClick={() => {
+                setExpand(!expand)
+              }}
+            >
+              <ExpandMoreIcon
+                className={clsx({
+                  [classes.expandMore]: !expand,
+                  [classes.expandLess]: expand,
+                })}
+              />
+            </IconButton>
+          </ListItemSecondaryAction>
+        )}
+      </ListItem>
+      <Collapse in={expand} timeout="auto" unmountOnExit>
+        <Paper
+          square
+          elevation={0}
+          className={classes.mergeOptions}
+        >
+          <RadioGroup size='small' value={step.mergeBehaviour} onChange={e => {
+            if (e.target.value === 'discard local changes') {
+              editorDispatch(setMergeBehaviourDiscardLocal(stepId))
+            }
+            else if (e.target.value === 'merge') {
+              editorDispatch(setMergeBehaviourMerge(stepId))
+            }
+          }}>
+            <FormControlLabel
+              value='discard local changes'
+              label="Discard local changes"
+              disabled={!isCurrent}
+              control={<Radio size='small' className={classes.radioButton} />}
+              classes={{
+                label: classes.smallFont
+              }}
+            />
+            <FormControlLabel
+              value='merge'
+              label="Merge"
+              disabled={!isCurrent}
+              control={<Radio size='small' className={classes.radioButton} />}
+              classes={{
+                label: classes.smallFont
+              }}
+            />
+          </RadioGroup>
+        </Paper>
+      </Collapse>
+    </>
+  )
 }
 
 export const StepItem = memo(({ id }) => {
