8abf6f56ef7d343adcff7d0beb29dea3722b8a80
michaelyinopen <michaelyinopen@gmail.com>
2022-04-23 21:03:51 +1000

    Remove Azure constants from the home page

--- src/hostsConstants.tsx
+++ src/hostsConstants.tsx
@@ -2,70 +2,13 @@ import { useState, useEffect } from 'react'
 import { format } from 'date-fns'
 import type { ReactNode } from 'react'
 
-export type Hosts = "azure" | "linode"
+export type Hosts = "linode"
 
 export type UseBuiltWiths = () => ReactNode[]
 export type HostConstants = {
   useBuiltWiths: UseBuiltWiths
 }
 export type HostsConstants = Record<Hosts, HostConstants>
-
-const useBuiltWithsAzure: UseBuiltWiths = () => {
-  const [apiLastDeployedDate, setApiLastDeployedDate] = useState("")
-  useEffect(() => {
-    const fetchApiLastDeployedDate = async () => {
-      try {
-        const response = await fetch(`https://api.github.com/repos/michaelyinopen/job-shop-collection-api/actions/workflows/main_job-shop-collection-api.yml/runs?per_page=1&status=success`)
-        if (!response.ok) {
-          console.log("Failed to get api last deployed date")
-          return
-        }
-        const responseBody = await response.json()
-        const apiLastDeploymentDate = new Date(responseBody.workflow_runs[0].updated_at)
-        setApiLastDeployedDate(" on " + format(apiLastDeploymentDate, 'yyyy-MM-dd'))
-      }
-      catch (e) {
-        console.log("Failed to get api last deployed date")
-        return
-      }
-    }
-    fetchApiLastDeployedDate()
-  }, [])
-  const [webLastDeployedDate, setWebLastDeployedDate] = useState("")
-  useEffect(() => {
-    const fetchWebLastDeployedDate = async () => {
-      try {
-        const response = await fetch(`https://api.github.com/repos/michaelyinopen/job-shop-collection-web/actions/workflows/main_azure.yml/runs?per_page=1&status=success`)
-        if (!response.ok) {
-          console.log("Failed to get web last deployed date")
-          return
-        }
-        let responseBody = await response.json()
-        const webLastDeploymentDate = new Date(responseBody.workflow_runs[0].updated_at)
-        setWebLastDeployedDate(" on " + format(webLastDeploymentDate, 'yyyy-MM-dd'))
-      }
-      catch (e) {
-        console.log("Failed to get web last deployed date")
-        return
-      }
-    }
-    fetchWebLastDeployedDate()
-  }, [])
-  return [
-    (<>
-      Web API and Database hosted on <a href="https://azure.microsoft.com/en-au/services/app-service/" target="_blank" rel="noreferrer">Azure Web Service</a> and <a href="https://azure.microsoft.com/en-au/services/sql-database/" target="_blank" rel="noreferrer">Azure SQL Database</a><br />
-      Continuously deployed {apiLastDeployedDate} with <a href='https://github.com/michaelyinopen/job-shop-collection-api/actions/workflows/main_job-shop-collection-api.yml' target="_blank" rel="noreferrer">this Github Actions workflow</a>
-    </>),
-    (<>
-      React App hosted on <a href="https://azure.microsoft.com/en-au/services/storage/blobs/" target="_blank" rel="noreferrer">Azure Blob Storage</a> with a CDN<br />
-      Continuously deployed {webLastDeployedDate} with <a href='https://github.com/michaelyinopen/job-shop-collection-web/actions/workflows/main_azure.yml' target="_blank" rel="noreferrer">this Github Actions workflow</a>
-    </>),
-  ]
-}
-
-const azureConstants: HostConstants = {
-  useBuiltWiths: useBuiltWithsAzure
-}
 
 const useBuiltWithsLinode: UseBuiltWiths = () => {
   const [apiLastDeployedDate, setApiLastDeployedDate] = useState("")
@@ -127,6 +70,5 @@ const linodeConstants: HostConstants = {
 }
 
 export const hostsConstants: HostsConstants = {
-  azure: azureConstants,
   linode: linodeConstants
 }
